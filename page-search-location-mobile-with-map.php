3e0889ba80bf9261c0ee0265242445a30e884862
kyleyiyang <kyleyiyang@gmail.com>
2020-06-07 15:57:08 -0400

    Create page-search-location-mobile-with-map.php

---
+++ page-search-location-mobile-with-map.php
@@ -0,0 +1,8 @@
+Make sure the mobile version of the site has the google maps (which should also show up at the top of the page on search results on mobile)
+Moved the google map div from bottom up to be on top of search results:
+
+<div class="search-location-line"></div>
+	  <div id="google_canvas_wrap">
+    <div id="google_canvas"></div>
+</div>
+Saved in: /page-search-location-mobile-with-map.php
